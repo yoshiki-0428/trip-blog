087174937404925bb23dd8dcd000a2b8592f589c
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-02-05 12:52:31 +0900

    fix

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -12,6 +12,17 @@ import { SinglePostTemplate } from '../templates/SinglePost'
 
 CMS.registerMediaLibrary(uploadcare)
 
+if (
+  window.location.hostname === 'localhost' &&
+  window.localStorage.getItem('netlifySiteURL')
+) {
+  CMS.registerPreviewStyle(
+    window.localStorage.getItem('netlifySiteURL') + '/styles.css'
+  )
+} else {
+  CMS.registerPreviewStyle('/styles.css')
+}
+
 CMS.registerPreviewTemplate('home-page', ({ entry }) => (
   <HomePageTemplate {...entry.toJS().data} />
 ))
