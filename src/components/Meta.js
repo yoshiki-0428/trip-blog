24a95f94a83da12fe85668cdc2cbe6f34da2b994
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-05-07 00:37:56 +0900

    fix twitter meta

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -58,7 +58,7 @@ export default class Meta extends Component {
         <meta name="twitter:image" content={absoluteImageUrl} />
         <meta property="og:image:secure_url" content={absoluteImageUrl} />
         <meta property="og:image" content={absoluteImageUrl} />
-        <meta name="twitter:card" content={absoluteImageUrl} />
+        <meta name="twitter:card" content="summary_large_image" />
 
         {googleTrackingId && (
           <script
