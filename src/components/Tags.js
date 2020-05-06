fa0257662f6a52dab089a705ea58d48eb02a8e9a
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-05-06 23:43:04 +0900

    fix tags css

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -4,15 +4,11 @@ import { kebabCase } from 'lodash/string'
 import React from 'react'
 import './Tags.css'
 
-const sortTotalCount = (tags) => orderBy(tags, ['totalCount'], ['desc'])
-const splitTags = (tags, condition) => tags.filter(tag => condition(tag))
+const sortTotalCount = (tags) => orderBy(tags, ['totalCount', 'fieldValue'], ['desc'])
 
 export const Tags = ({ tags, selectedTag }) => {
-  const oneContainTags = splitTags(tags, (tag) => tag.totalCount === 1).slice(0, 10)
-  const twoOverTags = splitTags(tags, (tag) => tag.totalCount > 1)
-  const unionTags = sortTotalCount(twoOverTags.concat(oneContainTags))
   return <div className={"Tags"}>
-    {unionTags.map(tag => (
+    {sortTotalCount(tags).map(tag => (
       <li key={tag.fieldValue}>
         <Link to={`/tags/${kebabCase(tag.fieldValue)}/`} className={selectedTag === tag.fieldValue ? 'Tags--Selected' : '' }>
           {tag.fieldValue}
